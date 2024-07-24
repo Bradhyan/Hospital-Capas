6e4fb4b974036826f2db4748dfe4bbcae3faec54
Bradhyan <143909229+Bradhyan@users.noreply.github.com>
2024-07-23 23:41:38 -0500

    Add files via upload

---
+++ CEntidad/CECitas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System Cdatos;
+
+namespace CEntidad
+{
+    public class CECitas
+    {
+        private String cod_cita;
+        private DateTime fecha;
+        private DateTime hora;
+        private String idPaciente;
+        private String id_medico;
+        private int valor;
+        private String diagnostico;
+        private String nom_acompañante;
+        private byte activo;
+
+        public string Cod_cita { get => cod_cita; set => cod_cita = value; }
+        public DateTime Fecha { get => fecha; set => fecha = value; }
+        public DateTime Hora { get => hora; set => hora = value; }
+        public string IdPaciente { get => idPaciente; set => idPaciente = value; }
+        public string Id_medico { get => id_medico; set => id_medico = value; }
+        public int Valor { get => valor; set => valor = value; }
+        public string Diagnostico { get => diagnostico; set => diagnostico = value; }
+        public string Nom_acompañante { get => nom_acompañante; set => nom_acompañante = value; }
+        public byte Activo { get => activo; set => activo = value; }
+    }
+}
---
+++ CEntidad/CEPacientes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CEntidad
+{
+   public class CEPacientes
+    {
+        private String id_paciente;
+        private String nom_paciente;
+        private String dir_paciente;
+        private String tel_paciente;
+        private String cel_paciente;
+        private byte activo;
+
+        public string Id_paciente { get => id_paciente; set => id_paciente = value; }
+        public string Nom_paciente { get => nom_paciente; set => nom_paciente = value; }
+        public string Dir_paciente { get => dir_paciente; set => dir_paciente = value; }
+        public string Tel_paciente { get => tel_paciente; set => tel_paciente = value; }
+        public string Cel_paciente { get => cel_paciente; set => cel_paciente = value; }
+        public byte Activo { get => activo; set => activo = value; }
+    }
+}
